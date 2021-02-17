59367fadf16b286720e8d6727e351ef18c61ec49
osipowsky <71651812+osipowsky@users.noreply.github.com>
2021-02-17 19:38:18 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.getElementById('btn').onclick = function () {
+   document.getElementById('menu').classList.toggle('header__menu-active');
+};
+// document.getElementById('btn').onclick = function () {
+//    document.getElementById('btn').classList.toggle('btn-active');
+// };
